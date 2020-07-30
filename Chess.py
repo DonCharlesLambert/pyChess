59231f0125878a4d39f651939279005775d3f4b1
Don Charles-Lambert <zcabdc0@ucl.ac.uk>
2020-07-31 00:12:43 +0100

    (00:12) A board with many tiles

--- Chess.py
+++ Chess.py
@@ -3,15 +3,26 @@ from PIL import Image
 from PIL import ImageTk
 
 class Chess:
-    WIDTH = 400
-    HEIGHT = 400
+    WIDTH = 400 # Square
+    BOARD_SIZE = 360
+    BOARD_START = 20
+    TILE_SIZE = int(BOARD_SIZE/8)
 
     def __init__(self):
         window = Tk()
-        self.canvas = Canvas(window, width=self.WIDTH, height=self.HEIGHT)
+        self.canvas = Canvas(window, width=self.WIDTH, height=self.WIDTH)
         self.canvas.pack()
-        self.board = BoardTile(self.canvas, 0, x=200, y=200)
+        self.board = []
+        self.create_board()
         window.mainloop()
+
+    def create_board(self):
+        tile = 0
+        for x in range(self.BOARD_START, self.BOARD_START + self.BOARD_SIZE, self.TILE_SIZE):
+            for y in range(self.BOARD_START, self.BOARD_START + self.BOARD_SIZE, self.TILE_SIZE):
+                self.board.append(BoardTile(self.canvas, tile, x=x, y=y, size=self.TILE_SIZE))
+                tile = (tile + 1) % 2
+            tile = (tile + 1) % 2
 
 
 class BoardTile:
@@ -28,9 +39,9 @@ class BoardTile:
             image = 'img/grey_light_square.png'
         elif tile == self.DARK_GREY:
             image = 'img/grey_dark_square.png'
-        self.sprite = self.resize(image, int(400/8), int(400/8))
+        self.sprite = self.resize(image, int(360/8), int(360/8))
         self.canvas = canvas
-        self.canvas.create_image(kwargs['x'], kwargs['y'], image=self.sprite)
+        self.canvas.create_image(kwargs['x'], kwargs['y'], image=self.sprite, anchor="nw")
 
     @staticmethod
     def resize(directory, width, height):
